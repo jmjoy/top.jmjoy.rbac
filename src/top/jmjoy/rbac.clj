c25c825835e42f624b507af7608adc0a83a10a31
jmjoy <jmjoy@jmjoy.top>
2017-07-09 12:56:39 +0800

    First commit.

---
+++ src/top/jmjoy/rbac.clj
@@ -0,0 +1,6 @@
+(ns top.jmjoy.rbac)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/top/jmjoy/rbac_test.clj
@@ -0,0 +1,7 @@
+(ns top.jmjoy.rbac-test
+  (:require [clojure.test :refer :all]
+            [top.jmjoy.rbac :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
